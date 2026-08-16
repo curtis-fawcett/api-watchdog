4a55f40f26a6762aecb0c8a26b44f940027b2346
TruffleShuffle1024 <fawcett.curtis@gmail.com>
2026-08-15 22:42:48 -0400

    Refactor API test into function

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import requests
 
-url = input("Enter API URL: ").strip()
+def test_api(url):
 
-if url == "":
-    print("URL cannot be empty")
-
-else:
-
-    try:
-        response = requests.get(url, timeout=5)
-
-    except requests.exceptions.ConnectionError:
-        print("Connection failed")
-    except requests.exceptions.Timeout:
-        print("Connection timed out")
-    except requests.exceptions.MissingSchema:
-        print("Invalid URL. Include http:// or https://")
+    if url == "":
+        print("URL cannot be empty")
 
     else:
-        print("Status Code:", response.status_code)
 
-        if response.status_code == 200:
-            print("PASS")
+        try:
+            response = requests.get(url, timeout=5)
+
+        except requests.exceptions.ConnectionError:
+            print("Connection failed")
+        except requests.exceptions.Timeout:
+            print("Connection timed out")
+        except requests.exceptions.MissingSchema:
+            print("Invalid URL. Include http:// or https://")
+
         else:
-            print("FAIL")
+            print("Status Code:", response.status_code)
 
-        response_time = int(response.elapsed.total_seconds() * 1000)
+            if response.status_code == 200:
+                print("PASS")
+            else:
+                print("FAIL")
 
-        print("Response Time:" , response_time, "ms")
+            response_time = int(response.elapsed.total_seconds() * 1000)
+
+            print("Response Time:" , response_time, "ms")
+
+url = input("Enter API URL: ").strip()
+
+test_api(url)
