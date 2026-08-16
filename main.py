64ed6775566f6f0f8d44be827d8510dca3f8e310
TruffleShuffle1024 <fawcett.curtis@gmail.com>
2026-08-16 10:57:47 -0400

    Return API test results from function

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 import requests
 
 def test_api(url):
-
     if url == "":
         print("URL cannot be empty")
-
+        return None, None, None
     else:
-
         try:
             response = requests.get(url, timeout=5)
-
         except requests.exceptions.ConnectionError:
             print("Connection failed")
+            return None, None, None
         except requests.exceptions.Timeout:
             print("Connection timed out")
+            return None, None, None
         except requests.exceptions.MissingSchema:
             print("Invalid URL. Include http:// or https://")
-
+            return None, None, None
         else:
-            print("Status Code:", response.status_code)
-
             if response.status_code == 200:
-                print("PASS")
+                result = "PASS"
             else:
-                print("FAIL")
-
+                result = "FAIL"
             response_time = int(response.elapsed.total_seconds() * 1000)
-
-            print("Response Time:" , response_time, "ms")
-
+            return result, response.status_code, response_time
 url = input("Enter API URL: ").strip()
-
-test_api(url)
+test_result, status_code, response_time = test_api(url)
+if test_result is not None:
+    print("Result:", test_result)
+    print("Status Code:", status_code)
+    print("Response Time:", response_time, "ms")
